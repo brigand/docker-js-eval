8fdc2d1455ec559d0100aa3a877bb3d87981552f
Frankie Bagnardi <f.bagnardi@gmail.com>
2017-01-28 14:36:58 -0700

    upgrades deps

--- eval-js.js
+++ eval-js.js
@@ -5,7 +5,7 @@ if (process.argv.indexOf('--use-babel') !== -1) {
   var babel = require('babel-standalone');
   require('babel-polyfill');
   code = babel.transform(code, {
-    presets: ['es2015', 'stage-1']
+    presets: ['es2015', 'es2016', 'stage-1']
   }).code;
 }
 console.log(inspect(eval(code)));
